a68152b72c94c46cc34b34f80b76500d1b18e5c4
cchromak <cchromak@gmail.com>
2020-07-02 10:56:50 -0400

    Added arrangeCoins method

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        WeekOne coin = new WeekOne();
+        System.out.println(coin.arrangeCoins(6));
+
+    }
+}
