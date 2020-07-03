80d24a0da7693ac23b2ca4f161c6601d91f40ecc
cchromak <cchromak@gmail.com>
2020-07-03 12:37:31 -0400

    Added prisonAfterNDays method and occupied helper method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,18 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         WeekOne coin = new WeekOne();
         System.out.println(coin.arrangeCoins(6));
+
+        WeekOne cells = new WeekOne();
+        int[] jail = {0,1,0,1,1,0,0,1};
+        int x = 7;
+        System.out.println(Arrays.toString(cells.prisonAfterNDays(jail, x)));
 
     }
 }
--- src/com/company/WeekOne.java
+++ src/com/company/WeekOne.java
@@ -96,6 +96,71 @@ public class WeekOne {
     */
 
 
+/*  3)
+
+    There are 8 prison cells in a row, and each cell is either occupied or vacant.
+
+    Each day, whether the cell is occupied or vacant changes according to the following rules:
+
+    If a cell has two adjacent neighbors that are both occupied or both vacant, then the cell becomes occupied.
+    Otherwise, it becomes vacant.
+            (Note that because the prison is a row, the first and the last cells in the row can't have two adjacent neighbors.)
+
+    We describe the current state of the prison in the following way: cells[i] == 1 if the i-th cell is occupied, else cells[i] == 0.
+
+    Given the initial state of the prison, return the state of the prison after N days (and N such changes described above.)
+
+    Input: cells = [0,1,0,1,1,0,0,1], N = 7
+    Output: [0,0,1,1,0,0,0,0]
+    Explanation:
+    The following table summarizes the state of the prison on each day:
+    Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+    Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+    Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+    Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+    Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+    Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+    Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+    Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+    */
+
+
+    public int[] prisonAfterNDays(int[] cells, int N) {
+        if (N == 0) {
+            return cells;
+        }
+        int[] temp = new int[cells.length];
+        N = N % 14 == 0 ? 14 : N % 14;
+
+        while (N-- > 0) {
+            for (int i = 0; i < cells.length; i++) {
+                if(occupied(cells, i)) {
+                    temp[i] = 1;
+                } else {
+                    temp[i] = 0;
+                }
+            }
+
+            for (int i = 0; i < cells.length; i++) {
+                cells[i] = temp[i];
+            }
+        }
+
+        return cells;
+    }
+
+    public boolean occupied(int[] cellArr, int i) {
+        if (i == 0 || i == cellArr.length - 1) {
+            return false;
+        }
+        if ((cellArr[i - 1] == 0 && cellArr[i + 1] == 0) || (cellArr[i - 1] == 1 && cellArr[i + 1] == 1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 
 
 
