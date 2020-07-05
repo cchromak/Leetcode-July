60c9c3ce53679f500ad6b7cc31c0c9172d5c3bde
cchromak <cchromak@gmail.com>
2020-07-05 10:44:07 -0400

    Added nthUglyNumber method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-/*        WeekOne coin = new WeekOne();
+        // 1)
+        WeekOne coin = new WeekOne();
         System.out.println(coin.arrangeCoins(6));
 
+
+        // 2)
         WeekOne cells = new WeekOne();
         int[] jail = {0,1,0,1,1,0,0,1};
         int x = 7;
-        System.out.println(Arrays.toString(cells.prisonAfterNDays(jail, x)));*/
+        System.out.println(Arrays.toString(cells.prisonAfterNDays(jail, x)));
+
+        // 4)
+        WeekOne ugly = new WeekOne();
+        System.out.println(ugly.nthUglyNumber(10));
 
 
 
