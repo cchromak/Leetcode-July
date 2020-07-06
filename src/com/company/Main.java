b0cacb9fbe5c4e6cb3172da597a60bf9c7d4ada5
cchromak <cchromak@gmail.com>
2020-07-06 14:42:10 -0400

    Added plusOne method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,26 +9,41 @@ public class Main {
     public static void main(String[] args) {
 
         // 1)
+        System.out.println("-------------------------------");
+        System.out.println("Day 1");
         WeekOne coin = new WeekOne();
         System.out.println(coin.arrangeCoins(6));
 
 
         // 2)
+        System.out.println("-------------------------------");
+        System.out.println("Day 2");
         WeekOne cells = new WeekOne();
         int[] jail = {0,1,0,1,1,0,0,1};
         int x = 7;
         System.out.println(Arrays.toString(cells.prisonAfterNDays(jail, x)));
 
         // 4)
+        System.out.println("-------------------------------");
+        System.out.println("Day 4");
         WeekOne ugly = new WeekOne();
         System.out.println(ugly.nthUglyNumber(10));
 
         // 5)
+        System.out.println("-------------------------------");
+        System.out.println("Day 5");
         WeekOne bin = new WeekOne();
         int xham = 1;
         int yham = 4;
 
         System.out.println(bin.hammingDistance(xham, yham));
+
+        // 6)
+        System.out.println("-------------------------------");
+        System.out.println("Day 6");
+        WeekOne addDigit = new WeekOne();
+        int[] number = {9, 9, 9, 9, 9, 9};
+        System.out.println(Arrays.toString(addDigit.plusOne(number)));
 
 
 
--- src/com/company/WeekOne.java
+++ src/com/company/WeekOne.java
@@ -240,9 +240,66 @@ public class WeekOne {
     }
 
 
+    /* 6)
+
+    Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+    The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+    You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+            Example 1:
+
+    Input: [1,2,3]
+    Output: [1,2,4]
+    Explanation: The array represents the integer 123.
+    Example 2:
+
+    Input: [4,3,2,1]
+    Output: [4,3,2,2]
+    Explanation: The array represents the integer 4321.*/
+
+    public int[] plusOne(int[] digits) {
+        int end = digits.length - 1;
+        if (digits[end] != 9) {
+            digits[end]++;
+            return digits;
+        }
+        if (digits.length == 1 && digits[0] == 9) {
+            int[] smallResult = new int[2];
+            smallResult[0] = 1;
+            smallResult[1] = 0;
+            return smallResult;
+        }
+        for (int i = 1; i <= end; end--) {
+            if (digits[end] == 9 || digits[end] == 10) {
+                digits[end] = 0;
+                digits[end - 1]++;
+                if (digits[end - 1] < 10) {
+                    break;
+                }
+            }
+        }
+        if (digits[0] == 10) {
+            int[] extendedResult = new int[digits.length + 1];
+            extendedResult[0] = 1;
+                for (int i = 1; i < extendedResult.length; i++) {
+                    extendedResult[i] = 0;
+                }
+            return extendedResult;
+        }
+        return digits;
+    }
+
+
+
+
+    }
 
 
 
 
 
-}
+
+
+
